47520e74655a2990a04cff54ffcd076fadb0c97e
Oleg Derevyanko <trekol39042@gmail.com>
2021-12-01 21:01:58 +0200

    Day 1;

---
+++ src/day1/day1.kt
@@ -0,0 +1,33 @@
+fun main() {
+    partTwo()
+}
+
+fun partOne() {
+    val input = readInput("/day1/input").map { it.toInt() }
+
+    val answer = greaterThanPreviousCount(input)
+
+    println(answer)
+}
+
+fun partTwo() {
+    val input = readInput("/day1/input").map { it.toInt() }
+    val input_size = input.size
+
+    val sums = mutableListOf<Int>()
+    for (i in 0..input_size - 3) {
+        sums.add(input[i] + input[i + 1] + input[i + 2])
+    }
+
+    val answer = greaterThanPreviousCount(sums)
+    println(answer)
+}
+
+fun greaterThanPreviousCount(input: List<Int>): Int {
+    val booleanList = input.indices.map { index ->
+        if (index == 0) false
+        else input[index - 1] < input[index]
+    }
+    val answer = booleanList.count { it }
+    return answer
+}
